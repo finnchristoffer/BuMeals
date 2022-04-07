0981ebf4a1b906648328d17f9da178907faba4bf
BuMeals - Group4 - ShiftPagi <finn.christoffer@gmail.com>
2022-04-07 11:19:30 +0700

    Initial Commit

---
+++ BuMeals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuMeals
+//
+//  Created by Finn Christoffer Kurniawan on 06/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
