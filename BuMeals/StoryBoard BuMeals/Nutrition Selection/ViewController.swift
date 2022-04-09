0d701ad8e982f9db8215443dc1c1e543e816e91d
finnchristoffer <finn.christoffer@gmail.com>
2022-04-09 20:55:54 +0700

    nutrisi selection v1

--- BuMeals/StoryBoard BuMeals/Nutrition Selection/ViewController.swift
+++ BuMeals/StoryBoard BuMeals/Nutrition Selection/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var Protein: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
