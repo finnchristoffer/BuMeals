9700836ab493aa83746ffe5754d58f832aa5f409
Jason Puwardi <jason.puwardi@binus.ac.id>
2022-04-11 09:54:07 +0700

    ui welcome page

---
+++ BuMeals/StoryBoard BuMeals/Welcome Page/Launch.swift
@@ -0,0 +1,24 @@
+//
+//  Launch.swift
+//  BuMeals
+//
+//  Created by Jason Puwardi on 08/04/22.
+//
+
+import Foundation
+import UIKit
+
+class Launch: UIViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
