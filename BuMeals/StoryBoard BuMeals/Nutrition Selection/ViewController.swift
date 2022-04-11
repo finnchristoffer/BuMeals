9a47aa5e5f908ce7d3ad5e69654217e8d291881d
finnchristoffer <finn.christoffer@gmail.com>
2022-04-11 09:52:33 +0700

    update version 4

--- BuMeals/StoryBoard BuMeals/Nutrition Selection/ViewController.swift
+++ BuMeals/StoryBoard BuMeals/Nutrition Selection/ViewController.swift
@@ -10,12 +10,141 @@ import UIKit
 class ViewController: UIViewController {
 
     
+   var arrOfPilihanUser:[JenisZatGizi]=[JenisZatGizi.Protein,JenisZatGizi.VitaminB2]
+   var tampilUser:[BahanMakanan]=[]
+
+    
     @IBOutlet weak var Protein: UIButton!
+    @IBOutlet weak var VitaminA: UIButton!
+    @IBOutlet weak var VitaminB1: UIButton!
+    @IBOutlet weak var VitaminB2: UIButton!
+    @IBOutlet weak var VitaminB3: UIButton!
+    @IBOutlet weak var VitaminB6: UIButton!
+    @IBOutlet weak var VitaminB9: UIButton!
+    @IBOutlet weak var VitaminB12: UIButton!
+    @IBOutlet weak var VitaminC: UIButton!
+    @IBOutlet weak var VitaminD: UIButton!
+    @IBOutlet weak var VitaminE: UIButton!
+    @IBOutlet weak var VitaminK: UIButton!
+    @IBOutlet weak var Kalsium: UIButton!
+    @IBOutlet weak var Fosfor: UIButton!
+    @IBOutlet weak var Magnesium: UIButton!
+    @IBOutlet weak var ZatBesi: UIButton!
+    @IBOutlet weak var Yodium: UIButton!
+    @IBOutlet weak var Zinc: UIButton!
+    @IBOutlet weak var Selenium: UIButton!
+    @IBOutlet weak var Mangan: UIButton!
+    @IBOutlet weak var Selanjutnya: UIButton!
+    @IBOutlet weak var infoProtein: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Rounded Button
+        Selanjutnya.layer.cornerRadius = 25
+        Selanjutnya.layer.masksToBounds = true
+        
+        //Init Symbol Info
+        //infoProtein.setImage(UIImage(systemName: "info.circle"), for: .normal)
+        
+    }
+    
+    //
+    func tampilUserChecker(arrBahanMakanan:[JenisZatGizi],arrOfPilihanUser:[BahanMakanan]) -> Void{
+        for itemBahan in arrBahanMakanan {
+            for itemPilihan in arrOfPilihanUser{
+                if itemPilihan.checkIfZatGiziExists(jenisZatGizi: itemBahan){
+                    tampilUser.append(itemPilihan)
+                }
+            }
+        }
     }
 
+    
+    //error button
+    @IBAction func pressProtein(_ sender: Any) {
+        Protein.alpha = 0.4
+    }
+    
+    @IBAction func pressVitA(_ sender: Any) {
+        VitaminA.alpha = 0.4
+    }
 
+    @IBAction func pressVitB1(_ sender: Any) {
+        VitaminB1.alpha = 0.4
+    }
+    
+    @IBAction func pressVitB2(_ sender: Any) {
+        VitaminB2.alpha = 0.4
+    }
+    
+    @IBAction func pressVitB3(_ sender: Any) {
+        VitaminB3.alpha = 0.4
+    }
+    
+    @IBAction func pressVitB6(_ sender: Any) {
+        VitaminB6.alpha = 0.4
+    }
+    
+    @IBAction func pressVitB9(_ sender: Any) {
+        VitaminB9.alpha = 0.4
+    }
+    //error button
+    @IBAction func pressVitB12(_ sender: Any) {
+        VitaminB12.alpha = 0.4
+    }
+    
+    @IBAction func pressVitC(_ sender: Any) {
+        VitaminC.alpha = 0.4
+    }
+    
+    @IBAction func pressVitD(_ sender: Any) {
+        VitaminD.alpha = 0.4
+    }
+    
+    @IBAction func pressVitE(_ sender: Any) {
+        VitaminE.alpha = 0.4
+    }
+    
+    @IBAction func pressVitK(_ sender: Any) {
+        VitaminK.alpha = 0.4
+    }
+    
+    @IBAction func pressKalsium(_ sender: Any) {
+        Kalsium.alpha = 0.4
+    }
+    
+    @IBAction func pressFosfor(_ sender: Any) {
+        Fosfor.alpha = 0.4
+    }
+    
+    @IBAction func pressMagnesium(_ sender: Any) {
+        Magnesium.alpha = 0.4
+    }
+    
+    @IBAction func pressZatBesi(_ sender: Any) {
+        ZatBesi.alpha = 0.4
+    }
+    
+    @IBAction func pressYodium(_ sender: Any) {
+        Yodium.alpha = 0.4
+    }
+    
+    @IBAction func pressZinc(_ sender: Any) {
+        Zinc.alpha = 0.4
+    }
+    
+    @IBAction func pressSelenium(_ sender: Any) {
+        Selenium.alpha = 0.4
+    }
+    
+    @IBAction func pressMangan(_ sender: Any) {
+        Mangan.alpha = 0.4
+    }
+    
+    @IBAction func pressSelanjutnya(_ sender: Any) {
+        
+    }
 }
 
--- BuMeals/StoryBoard BuMeals/Object.swift
+++ BuMeals/StoryBoard BuMeals/Object.swift
@@ -12,10 +12,10 @@ let Protein=ZatGizi(titleGizi: "Protein", definisiGizi: "Pada ibu hamil, Protein
 let VitaminA=ZatGizi(titleGizi: "Vitamin A", definisiGizi: "Pada ibu hamil, Vitamin A berguna untuk proses perkembangan jantung, paru-paru, ginjal, mata, dan Tulang janin. Setelah melahirkan, vitamin A membantu memperbaiki jaringan tubuh ibu, menurunkan resiko terjadinya infeksi, memperkuat daya tahan tubuh, dan menjaga kesehatan penglihatan.")
 
 //Isi Bahan Makanan (pisang jpg error karena belom ada)
-// arrOfResep itu isinya object dari reseo makanan, contoh makanan Bayam brarti jadi arrOfresep:[BayamBeningJagung,]
+ //arrOfResep itu isinya object dari reseo makanan, contoh makanan Bayam brarti jadi arrOfresep:[BayamBeningJagung,]
 
-let Pisang=BahanMakanan(namaBahan: "Pisang", tipeBahan: "Bh1", gambarBahan: UIImage(named: "pisang.jpg"), countMenuPilihanBahan: 0, definisiBahan: "Buah", arrOfZatGizi: [JenisZatGizi.VitaminB6,JenisZatGizi.VitaminC],arrOfResep: [])
-let Bayam=BahanMakanan(namaBahan: "Bayam", tipeBahan: "Bh1", gambarBahan: UIImage(named: "pisang.jpg"), countMenuPilihanBahan: 0, definisiBahan: "Buah", arrOfZatGizi: [JenisZatGizi.VitaminB6,JenisZatGizi.VitaminC],arrOfResep: [])
+//let Pisang=BahanMakanan(namaBahan: "Pisang", tipeBahan: "Bh1", gambarBahan: UIImage(named: "pisang.jpg"), countMenuPilihanBahan: 0, definisiBahan: "Buah", arrOfZatGizi: [JenisZatGizi.VitaminB6,JenisZatGizi.VitaminC],arrOfResep: [])
+//let Bayam=BahanMakanan(namaBahan: "Bayam", tipeBahan: "Bh1", gambarBahan: UIImage(named: "pisang.jpg"), countMenuPilihanBahan: 0, definisiBahan: "Buah", arrOfZatGizi: [JenisZatGizi.VitaminB6,JenisZatGizi.VitaminC],arrOfResep: [])
 
 
 //isi Resep Makanan
