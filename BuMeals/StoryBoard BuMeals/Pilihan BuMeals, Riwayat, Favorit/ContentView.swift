173d491883860518838ebce66dc6837ee4c15f36
Evan Susanto <evan.susanto@ui.ac.id>
2022-04-12 20:34:55 +0700

    PilihanBuMeals,Favorit,Riwayat(SwiftUI V1)

---
+++ BuMeals/StoryBoard BuMeals/Pilihan BuMeals, Riwayat, Favorit/ContentView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+
+//var arrBahanMakanan:[BahanMakanan]=[Bayam,Bayam,Bayam]
+//var arrOfPilihanUser:[JenisZatGizi]=[JenisZatGizi.Protein,JenisZatGizi.VitaminB2]
+//var tampilUser:[BahanMakanan]=[]
+//func tampilUserChecker(arrBahanMakanan:[BahanMakanan],PilihanUser:JenisZatGizi) -> Void{
+//            for itemPilihan in arrBahanMakanan{
+//                if itemPilihan.checkIfZatGiziExists(jenisZatGizi: PilihanUser){
+//                    if tampilUser.contains(itemPilihan){
+//                        continue
+//                    }else{
+//                        tampilUser.append(itemPilihan)
+//                    }
+//
+//                }
+//        }
+//    }
+
+struct BahanMakananListView: View {
+    var tampilUser:[BahanMakanan]=[Pisang,Pisang,Pisang]
+    var body:some View {
+        VStack(alignment:.leading,spacing: 10){
+            NavigationView{
+                List(tampilUser,id: \.id){item in
+                    NavigationLink(destination:ContentDetailView(item: item),label:{
+                    Image(item.gambarBahan)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height:90)
+                        .cornerRadius(4)
+                    VStack(alignment: .leading, spacing: 5){
+                        Text(item.namaBahan)
+                            .fontWeight(.bold)
+                            .lineLimit(2)
+                            .font(.system(size: 20))
+                            .minimumScaleFactor(0.5)
+                        Text((item.tipeBahan+"/"+item.checkJenisZatGiziName(jenisZatGizi: item.arrOfZatGizi[0])))
+                            .font(.system(size: 15, weight:.light))
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.5)
+                        Text("\(String(item.countMenuPilihanBahan)) Menu Bahan")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+               )}
+                .navigationTitle("Pilihan BuMeals")
+                                       
+            }
+        }
+        
+        }
+    }
+
+
+
+struct ContentView_Previews:PreviewProvider{
+    static var previews: some View{
+        BahanMakananListView()
+    }
+}
---
+++ BuMeals/StoryBoard BuMeals/Pilihan BuMeals, Riwayat, Favorit/ListDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  testApp
+//
+//  Created by Evan Susanto on 11/04/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentDetailView:View{
+    var item:BahanMakanan
+    var body:some View{
+        VStack(spacing:20){
+            Image(item.gambarBahan)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 0, height: 50, alignment: .center)
+                .cornerRadius(12)
+            
+            Text(item.namaBahan)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            HStack{
+                Text(item.definisiBahan)
+            }
+            Spacer()
+            HStack(spacing:40){
+                Text("Baca")
+                    .frame(width: 50, height: 50, alignment: .leading)
+                Text("Evan Susanto Panjang")
+                    .frame(width: 50, height: 50, alignment: .bottom)
+            }
+            
+        }
+    }
+}
+
+struct ContentDetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentDetailView(item:SayurBayam)
+//    }
+}
