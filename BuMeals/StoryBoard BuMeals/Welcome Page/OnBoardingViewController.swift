1793e235bb7416e1020acf0b8be2cd88c9178ac1
Jason Puwardi <jason.puwardi@binus.ac.id>
2022-04-12 08:41:18 +0700

    update segue... perbaikin launch screen error

---
+++ BuMeals/StoryBoard BuMeals/Welcome Page/OnBoardingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LaunchViewController.swift
+//  BuMeals
+//
+//  Created by Jason Puwardi on 11/04/22.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    @IBOutlet weak var selanjutnya: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selanjutnya(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
