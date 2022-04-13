fd3fc49cd08b7e00f02b47637ac72bc5ea2348dd
finnchristoffer <finn.christoffer@gmail.com>
2022-04-13 10:58:39 +0700

    update sampai page bahan aman tinggal design rapihin

--- BuMeals/StoryBoard BuMeals/Nutrition Selection/ViewController.swift
+++ BuMeals/StoryBoard BuMeals/Nutrition Selection/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     fileprivate var contentViewInHC:UIHostingController<BahanMakananListView> = UIHostingController(rootView: BahanMakananListView(tampilUser: []))
     var arrBahanMakanan:[BahanMakanan]=[Pisang,Alpukat,Jeruk,Jagung,DagingSapi,DagingAyam,Salmon,Susu,Telur,Buncis,KacangTanah,KacangPolong,KacangKedelai,SayurBayam,SayurBrokoli,SayurWortel,Ubi ]
     var PilihanUser:JenisZatGizi?
-    var arrPilihanUser:[JenisZatGizi] = [JenisZatGizi.Protein,JenisZatGizi.VitaminA]
     var tampilUser:[BahanMakanan]=[]
     var selectedItems:[JenisZatGizi]=[]
     
@@ -75,28 +74,28 @@ class ViewController: UIViewController {
         }
     }
     
-    func tampilUserChecker(arrBahanMakanan:[BahanMakanan],PilihanUser:JenisZatGizi) -> Void{
-//                for itemPilihan in arrBahanMakanan{
-//                    if itemPilihan.checkIfZatGiziExists(jenisZatGizi: PilihanUser){
-//                        if tampilUser.contains(where: { makanan in
-//                            return makanan.checkIfZatGiziExists(jenisZatGizi: PilihanUser)
-//                        }){
-//                            print(itemPilihan)
-//                        }else{
-//                            tampilUser.append(itemPilihan)
-//                        }
-//
-                    //}
-            //}
-        tampilUser = arrBahanMakanan.filter( { makanan in
-                makanan.checkIfZatGiziExists(jenisZatGizi: PilihanUser)
-        })
-    }
+//    func tampilUserChecker(arrBahanMakanan:[BahanMakanan],PilihanUser:JenisZatGizi) -> Void{
+////                for itemPilihan in arrBahanMakanan{
+////                    if itemPilihan.checkIfZatGiziExists(jenisZatGizi: PilihanUser){
+////                        if tampilUser.contains(where: { makanan in
+////                            return makanan.checkIfZatGiziExists(jenisZatGizi: PilihanUser)
+////                        }){
+////                            print(itemPilihan)
+////                        }else{
+////                            tampilUser.append(itemPilihan)
+////                        }
+////
+//                    //}
+//            //}
+//        tampilUser = arrBahanMakanan.filter( { makanan in
+//                makanan.checkIfZatGiziExists(jenisZatGizi: PilihanUser)
+//        })
+//    }
     
     //error button
     @IBAction func pressProtein(_ sender: Any) {
         if selectionButtonProtein.alpha != 0.4{
-            selectedItems.append(JenisZatGizi.VitaminA)
+            selectedItems.append(JenisZatGizi.Protein)
             selectionButtonProtein.alpha = 0.4
 //            tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Protein)
             tampilUser1()
@@ -114,115 +113,132 @@ class ViewController: UIViewController {
         selectionButtonVitaminA.alpha = 0.4
 //        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminA)
         tampilUser1()
-        print(arrBahanMakanan[0].namaBahan)
+        print(tampilUser)
     }
 
     @IBAction func pressVitB1(_ sender: Any) {
         selectedItems.append(JenisZatGizi.VitaminB1)
         selectionButtonVitaminB1.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminB1)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitB2(_ sender: Any) {
         selectionButtonVitaminB2.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminB2)
+        selectedItems.append(JenisZatGizi.VitaminB2)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitB3(_ sender: Any) {
         selectionButtonVitaminB3.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminB3)
+        selectedItems.append(JenisZatGizi.VitaminB3)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitB6(_ sender: Any) {
         selectionButtonVitaminB6.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminB6)
+        selectedItems.append(JenisZatGizi.VitaminB6)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitB9(_ sender: Any) {
         selectionButtonVitaminB9.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminB9)
+        selectedItems.append(JenisZatGizi.VitaminB9)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitB12(_ sender: Any) {
         selectionButtonVitaminB12.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminB12)
+        selectedItems.append(JenisZatGizi.VitaminB12)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitC(_ sender: Any) {
         selectionButtonVitaminC.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminC)
+        selectedItems.append(JenisZatGizi.VitaminC)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitD(_ sender: Any) {
         selectionButtonVitaminD.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminD)
+        selectedItems.append(JenisZatGizi.VitaminD)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitE(_ sender: Any) {
         selectionButtonVitaminE.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminE)
+        selectedItems.append(JenisZatGizi.VitaminE)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressVitK(_ sender: Any) {
         selectionButtonVitaminK.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.VitaminK)
+        selectedItems.append(JenisZatGizi.VitaminK)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressKalsium(_ sender: Any) {
         selectionButtonKalsium.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Kalsium)
+        selectedItems.append(JenisZatGizi.Kalsium)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressFosfor(_ sender: Any) {
         selectionButtonFosfor.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Fosfor)
+        selectedItems.append(JenisZatGizi.Fosfor)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressMagnesium(_ sender: Any) {
         selectionButtonMagnesium.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Magnesium)
+        selectedItems.append(JenisZatGizi.Magnesium)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressZatBesi(_ sender: Any) {
         selectionButtonZatBesi.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.ZatBesi)
+        selectedItems.append(JenisZatGizi.ZatBesi)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressYodium(_ sender: Any) {
         selectionButtonYodium.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Yodium)
+        selectedItems.append(JenisZatGizi.Yodium)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressZinc(_ sender: Any) {
         selectionButtonZinc.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Yodium)
+        selectedItems.append(JenisZatGizi.Zinc)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressSelenium(_ sender: Any) {
         selectionButtonSelenium.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Selenium)
+        selectedItems.append(JenisZatGizi.Selenium)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
     @IBAction func pressMangan(_ sender: Any) {
         selectionButtonMangan.alpha = 0.4
-        tampilUserChecker(arrBahanMakanan: arrBahanMakanan, PilihanUser: JenisZatGizi.Mangan)
+        selectedItems.append(JenisZatGizi.Mangan)
+        tampilUser1()
         print(arrBahanMakanan[0].namaBahan)
     }
     
